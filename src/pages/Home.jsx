f5eb8593e1ba8dec291aacd9db28e24424f35c19
Pratap Padekar <unitedpratap7@gmail.com>
2023-06-09 00:50:26 +0530

    minor fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,35 @@ import { useEffect, useState } from "react"
 import Posts from "../components/Posts"
 import axios from 'axios'
 import {server} from '../constants/constants'
+import { Oval } from "react-loading-icons"
 
 const Home = () => {
   const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+
   useEffect(() =>{
     getAllPosts()
   }, [])
 
   const getAllPosts = async () =>{
+    setLoading(true)
     try {
       const {data} = await axios.get(`${server}/posts/all`)
+      setLoading(false)
       setPosts(data.posts)
     } catch (err) {
       console.log(err)
+      setLoading(false)
     } 
+  }
+
+  if(loading){
+    return (
+      <div className="fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+        <Oval height={'3rem'} stroke="#00c8ff" className="mx-auto" />
+        <p className="text-[#00c8ff]">Loading Posts...</p>
+      </div>
+    )
   }
 
   return (
