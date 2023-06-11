28c9c7e045532ad966118923d48af7c5b7bf193f
Pratap Padekar <unitedpratap7@gmail.com>
2023-06-11 12:40:53 +0530

    minor fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,23 +7,27 @@ import { Oval } from "react-loading-icons"
 const Home = () => {
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [totalPosts, setTotoalPosts] = useState(0)  
 
   useEffect(() =>{
     getAllPosts()
-  }, [])
+  }, [page])
 
   const getAllPosts = async () =>{
     setLoading(true)
     try {
-      const {data} = await axios.get(`${server}/posts/all`)
+      const {data} = await axios.get(`${server}/posts/all?page=${page}`)
       setLoading(false)
+      setTotoalPosts(data.postsCount)
+      console.log(data)
       setPosts(data.posts)
     } catch (err) {
       console.log(err)
       setLoading(false)
     } 
   }
-
+ 
   if(loading){
     return (
       <div className="fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
@@ -40,6 +44,10 @@ const Home = () => {
         {posts.length > 0 && posts.map((post) =>{
           return <Posts key={post._id} {...post} />
         })}
+       </div>
+       <div className="my-12 text-center">
+        <button disabled={page === 1} onClick={() => setPage(prev => prev - 1)} className="px-5 py-2 bg-[#555555] rounded-tl-full rounded-bl-full text-white border-r border-white disabled:opacity-60">&lt;</button>
+        <button disabled={page === Math.ceil(totalPosts / 8)} onClick={() => setPage(prev => prev + 1)} className="px-5 py-2 bg-[#555555] rounded-tr-full rounded-br-full text-white disabled:opacity-60">&gt;</button>
        </div>
     </div>
   )
